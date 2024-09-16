ac459d79bb63b44094bec2ebabef5f82f6cd5a79
MiniGT <bendanillo539@gmail.com>
2024-09-16 21:54:17 +0800

    change the source for challenge 3

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import App from './challenges/challenge3/App.tsx'
 // import App from './challenges/challenge1/App.tsx'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <App/>
   </StrictMode>,
 )
